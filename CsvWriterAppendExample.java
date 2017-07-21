95a0551a7c98ac68ee7dba539c2ce5285efc4665
liusongyang <liusong.yang@nationwide.com>
2017-07-21 00:25:37 -0400

    modified for AZ data input

--- CsvWriterAppendExample.java
+++ CsvWriterAppendExample.java
@@ -3,12 +3,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import com.csvreader.CsvWriter;
+import java.text.DecimalFormat;
 
 public class CsvWriterAppendExample {
 	
 	public static void main(String[] args) {
 		
-		String outputFile = "users.csv";
+		String outputFile = "state.csv";
 		
 		// before we open the file check to see if it already exists
 		boolean alreadyExists = new File(outputFile).exists();
@@ -24,28 +25,39 @@ public class CsvWriterAppendExample {
 				csvOutput.write("lon1");
 				csvOutput.write("lat2");
 				csvOutput.write("lon2");
+				csvOutput.write("seq");
 				csvOutput.endRecord();
 			}
-			// else assume that the file already has the correct header line
-			float start_lat = 40;
-			float end_lat = 50;
+			// tx + ok
+			/*double start_lat = 26;
+			double end_lat = 37;
 			
-			double start_lon = -80;
-			double end_lon = -70;
-			double increment_value = 0.2;
-			// write out a few records
+			double start_lon = -106.6;
+			double end_lon = -93.7;
+			double increment_value = 0.02;
+			int num = 1;*/
+			
+			//az
+			double start_lat = 31.3;
+			double end_lat = 36.95;
+			
+			double start_lon = -114.7;
+			double end_lon = -109;
+			double increment_value = 0.02;
+			int num = 355301;
 			
 			for ( double index_lat = start_lat; index_lat < end_lat; index_lat += increment_value )
 	          {
 				for ( double index_lon = start_lon; index_lon < end_lon; index_lon += increment_value )
 				{
 				
-				csvOutput.write(String.valueOf(index_lat));
-				csvOutput.write(String.valueOf(index_lon));
-				csvOutput.write(String.valueOf(index_lat + 0.2));
-				csvOutput.write(String.valueOf(index_lon + 0.2));
-				
+				csvOutput.write(new DecimalFormat("0.00").format(index_lat));
+				csvOutput.write(new DecimalFormat("0.00").format(index_lon));
+				csvOutput.write(new DecimalFormat("0.00").format(index_lat + increment_value));
+				csvOutput.write(new DecimalFormat("0.00").format(index_lon + increment_value));
+				csvOutput.write(String.valueOf(num));
 				csvOutput.endRecord();
+				num++;
 				}
 	          }	
 			
